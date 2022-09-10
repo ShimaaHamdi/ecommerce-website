b648fc78531f710bf9d3d6cc9655f5030a149883
ShimaaHamdi <108419926+ShimaaHamdi@users.noreply.github.com>
2022-09-10 04:16:22 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,32 @@
+const bar = document.getElementById("bar");
+const navbar = document.getElementById("navbar");
+const close = document.getElementById("close");
+const MainImg = document.getElementById("MainImg");
+const smallimg = document.getElementsByClassName("small-img");
+
+// navbar
+if (bar) {
+  bar.addEventListener("click", () => {
+    navbar.classList.add("active");
+  });
+}
+
+if (close) {
+  close.addEventListener("click", () => {
+    navbar.classList.remove("active");
+  });
+}
+
+// product img
+smallimg[0].onclick = function () {
+  MainImg.src = smallimg[0].src;
+};
+smallimg[1].onclick = function () {
+  MainImg.src = smallimg[1].src;
+};
+smallimg[2].onclick = function () {
+  MainImg.src = smallimg[2].src;
+};
+smallimg[3].onclick = function () {
+  MainImg.src = smallimg[3].src;
+};
